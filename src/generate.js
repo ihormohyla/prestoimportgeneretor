71d2e6a5776c425b1e3e6857703f00bab19c5a1b
ihormohyla <ihor.mohyla@cleveroad.com>
2022-09-28 16:15:21 +0300

    Refactor

--- src/generate.js
+++ src/generate.js
@@ -2,6 +2,7 @@ const args = require('minimist')(process.argv.slice(2));
 const axios = require('axios');
 const fastcsv = require('fast-csv');
 const fs = require('fs');
+const path = require('path');
 const { XMLParser } = require('fast-xml-parser');
 const categoriesModule = require('./categories');
 const productsModule = require('./products');
@@ -28,13 +29,17 @@ function writeCsv(data, fileName) {
 (async function main() {
     const url = args.url;
     const outDir = 'result';
+    console.log(path.resolve(outDir));
+    if (!fs.existsSync(path.resolve(outDir))) {
+        fs.mkdirSync(path.resolve( outDir));
+    }
     const data = await getDataFromUrl(url);
     let result = parser.parse(data.data);
     const {yml_catalog: {shop: { name, currencies: { currency }, categories: {category}, offers: {offer}}}} = result;
 
     const timestamp = Date.now();
-    const outputProductsFileName = `./${outDir}/${name}-products-${timestamp}.csv`
-    const outputCategoriesFileName = `./${outDir}/${name}-categories-${timestamp}.csv`
+    const outputProductsFileName = path.resolve(outDir, `${name}-products-${timestamp}.csv`);
+    const outputCategoriesFileName = path.resolve(outDir, `${name}-categories-${timestamp}.csv`);
 
     const catForWrite = categoriesModule.prepareCategories(category);
     writeCsv(catForWrite, outputCategoriesFileName);
